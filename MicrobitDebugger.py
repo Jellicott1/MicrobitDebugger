7f53b8a3deb02c85f3864ac30c7411c1b077f7e4
James Ellicott <jellicott21@gmail.com>
2021-05-10 22:44:27 +0100

    Separated Pin class into Digital, Analog and Touch Pins classes. Implemented relevant methods for these classes. Also, instantiated a MicroBit object and added module attributes/methods to relevant attributes/methods of the object.

--- MicrobitDebugger.py
+++ MicrobitDebugger.py
@@ -3,7 +3,7 @@ from time import time
 print("===== Python Debug Mode =====")
 
 
-class Button:
+class _Button:
 
     def __init__(self, name):
         self.name = name
@@ -17,13 +17,82 @@ class Button:
             return False
 
 
-class Pin:
+class _DigitalPin:
 
-    def __init__(self, idx, type, function):
+    def __init__(self, idx, function):
         self.id = idx
-        self.type = type
+        self.type = "digital"
+        self.function = function
+        self.value = 0
+
+    def read_digital(self):
+        value = input("Enter desired value (1 for high, 0 for low or nothing to take current value): ")
+        try:
+            if int(value) == 1 or int(value) == 0:
+                self.value = int(value)
+                return value
+            else:
+                return self.value
+        except ValueError:
+            return self.value
+
+    def write_digital(self, value):
+        if value == 1 or value == 0:
+            self.value = int(value)
+        else:
+            raise ValueError("Value must be either 1 or 0.")
+
+
+class _AnalogPin:
+
+    def __init__(self, idx, function):
+        self.id = idx
+        self.type = "analog"
+        self.function = function
+        self.value = 0
+        self.periodMicroSeconds = 256
+
+    def read_analog(self):
+        value = input("Enter desired voltage (0 to 1023 or nothing for current value): ")
+        try:
+            if 0 <= int(value) <= 1023:
+                return int(value)
+            else:
+                raise ValueError("Must be an integer between 0 and 1023.")
+        except ValueError:
+            return self.value
+
+    def write_analog(self, value):
+        if 0 <= value <= 1023:
+            self.value = int(value)
+        else:
+            raise ValueError("Value must be an integer between 0 and 1023.")
+
+    def set_analog_period(self, period):
+        if isinstance(period, int) and period >= 1:
+            self.periodMicroSeconds = 1000*period
+        else:
+            raise ValueError("Period must be an integer >= 1.")
+
+    def set_analog_period_microseconds(self, period):
+        if isinstance(period, int) and period >= 256:
+            self.periodMicroSeconds = period
+        else:
+            raise ValueError("Period must be an integer >= 256.")
+
+
+class _TouchPin:
+
+    def __init__(self, idx, function):
+        self.id = idx
+        self.type = "touch"
         self.function = function
 
+    def is_touched(self):
+        try:
+            return bool(input("Enter pin touched status (True/False)"))
+        except ValueError:
+            raise ValueError("Input must be a boolean.")
 
 class Image:
     EMPTY = "00000:" * 5
@@ -45,7 +114,7 @@ class Image:
         self.image[x + 6 * y] = value
 
 
-class Display:
+class _Display:
 
     def __init__(self, image=Image.EMPTY):
         self.image = Image()
@@ -81,7 +150,7 @@ class Display:
         return self.image.get_pixel(x, y)
 
 
-class MicroBit:
+class _MicroBit:
 
     @staticmethod
     def time_ms():
@@ -92,29 +161,29 @@ class MicroBit:
         self.startTime = self.time_ms()
         self.timeSlept = 0
 
-        self.display = Display()
+        self.display = _Display()
 
-        self.button_a = Button('A')
-        self.button_b = Button('B')
-        self.pin0 = Pin(0, "touch", "Pad 0")
-        self.pin1 = Pin(1, "touch", "Pad 1")
-        self.pin2 = Pin(2, "touch", "Pad 2")
-        self.pin3 = Pin(3, "analog", "Column 1")
-        self.pin4 = Pin(4, "analog", "Column 2")
-        self.pin5 = Pin(5, "digital", "Button A")
-        self.pin6 = Pin(6, "digital", "Column 9")
-        self.pin7 = Pin(7, "digital", "Column 8")
-        self.pin8 = Pin(8, "digital", None)
-        self.pin9 = Pin(9, "digital", "Column 7")
-        self.pin10 = Pin(10, "analog", "Column 3")
-        self.pin11 = Pin(11, "digital", "Button B")
-        self.pin12 = Pin(12, "digital", None)
-        self.pin13 = Pin(13, "digital", "SPI SCK")
-        self.pin14 = Pin(14, "digital", "SPI MISO")
-        self.pin15 = Pin(15, "digital", "SPI MOSI")
-        self.pin16 = Pin(16, "digital", None)
-        self.pin19 = Pin(19, "digital", "I2C SCL")
-        self.pin20 = Pin(20, "digital", "I2C SDA")
+        self.button_a = _Button('A')
+        self.button_b = _Button('B')
+        self.pin0 = _TouchPin(0, "Pad 0")
+        self.pin1 = _TouchPin(1, "Pad 1")
+        self.pin2 = _TouchPin(2, "Pad 2")
+        self.pin3 = _AnalogPin(3, "Column 1")
+        self.pin4 = _AnalogPin(4, "Column 2")
+        self.pin5 = _DigitalPin(5, "Button A")
+        self.pin6 = _DigitalPin(6, "Column 9")
+        self.pin7 = _DigitalPin(7, "Column 8")
+        self.pin8 = _DigitalPin(8, None)
+        self.pin9 = _DigitalPin(9, "Column 7")
+        self.pin10 = _AnalogPin(10, "Column 3")
+        self.pin11 = _DigitalPin(11, "Button B")
+        self.pin12 = _DigitalPin(12, None)
+        self.pin13 = _DigitalPin(13, "SPI SCK")
+        self.pin14 = _DigitalPin(14, "SPI MISO")
+        self.pin15 = _DigitalPin(15, "SPI MOSI")
+        self.pin16 = _DigitalPin(16, None)
+        self.pin19 = _DigitalPin(19, "I2C SCL")
+        self.pin20 = _DigitalPin(20, "I2C SDA")
 
     def panic(self, n):
         self.isPanicMode = True
@@ -137,3 +206,33 @@ class MicroBit:
 
     def temperature(self):
         return float(input("Enter desired temperate: "))
+
+
+_microbit = _MicroBit()
+display = _microbit.display
+button_a = _microbit.button_a
+button_b = _microbit.button_b
+pin0 = _microbit.pin0
+pin1 = _microbit.pin1
+pin2 = _microbit.pin2
+pin3 = _microbit.pin3
+pin4 = _microbit.pin4
+pin5 = _microbit.pin5
+pin6 = _microbit.pin6
+pin7 = _microbit.pin7
+pin8 = _microbit.pin8
+pin9 = _microbit.pin9
+pin10 = _microbit.pin10
+pin11 = _microbit.pin11
+pin12 = _microbit.pin12
+pin13 = _microbit.pin13
+pin14 = _microbit.pin14
+pin15 = _microbit.pin15
+pin16 = _microbit.pin16
+pin19 = _microbit.pin19
+pin20 = _microbit.pin20
+panic = _microbit.panic
+reset = _microbit.reset
+sleep = _microbit.sleep
+running_time = _microbit.running_time
+temperature = _microbit.running_time
